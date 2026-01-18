70b39079e201abee9a2695178e9b1b4fdb3912b8
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2026-01-18 10:09:47 +0530

    Add files via upload

---
+++ next_sml_ele_tow_rig.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> nextsmallerrightelement(vector<int>&arr,int n){
+    vector<int>v(n,-1);
+    stack<int>st;
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty() && st.top()>=arr[i]){
+            st.pop();
+        }
+        if(!st.empty()){
+            v[i]=st.top();
+        }
+        st.push(arr[i]);
+    }
+    return v;
+}
+int main(){
+    int n;
+    cout<<"enter no of elements:";
+    cin>>n;
+    vector<int>arr(n);
+    cout<<"enter elements:";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    vector<int> result;
+    result=nextsmallerrightelement(arr,n);
+    cout<<"next smaller right elements are:";
+    for(int i=0;i<result.size();i++){
+        cout<<result[i]<<" ";
+    }
+    return 0;
+}
