d532acc4dab1f8248999a39af90d8aab330d092d
BHUMIKA BAHUGUNA <158489676+BhumikaBahuguna@users.noreply.github.com>
2026-01-19 22:48:03 +0530

    Create maximal_rectangle.cpp

---
+++ maximal_rectangle.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int area(vector<int>heights){
+        int n=heights.size();
+            stack<int>st;
+            int maxarea=0;
+            for(int i=0;i<=n;i++){
+                int h=(i==n) ? 0 : heights[i];
+                while(!st.empty() && h<heights[st.top()]){
+                    int height=heights[st.top()];
+                    st.pop();
+                    int width=st.empty()?i:i-st.top()-1;
+                    maxarea=max(maxarea,height*width);
+                }
+                st.push(i);
+            }
+            return maxarea;
+        }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int m=matrix.size();
+        if(m==0) return 0;
+        int n=matrix[0].size();
+        vector<int>hist(n,0);
+        int ans=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]=='1')
+                    hist[j]++;
+                else
+                    hist[j]=0;
+            }
+            ans=max(ans,area(hist));
+        }
+    return ans;
+    }
+};
